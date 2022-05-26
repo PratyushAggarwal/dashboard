138db444b53246c9afbe93ee3a267fc7c969444d
PratyushAggarwal <pratyushaggarwal1281@gmail.com>
2022-05-26 14:56:41 +0530

    donut chart

---
+++ src/Components/Donutchart.js
@@ -0,0 +1,52 @@
+import { rgbToHex } from "@mui/material";
+import { width } from "@mui/system";
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+import ReactApexChart from "react-apexcharts";
+
+class ApexChart1 extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          series: [17, 41, 55, 44],
+          grid: {
+            chart: {
+                type: 'donut',
+                toolbar: {
+                  show: false
+                }
+            },
+            legend: {
+              show: false,
+            },
+            dataLabels: {
+              enabled: true,
+              background:{
+                enabled: false
+              }
+            },
+            labels: ["E-mail", "Sponsored", "Affiliate", "Direct"],
+            plotOptions: {
+              pie: {
+                customScale: 1.1
+              }
+            }
+          }
+        };
+      }
+
+    
+
+      render() {
+        return (
+          
+
+    <div id="chart">
+        <ReactApexChart options={this.state.grid} series={this.state.series} type="donut" height={205.7}/>
+    </div>
+        );
+    }
+}
+
+export default ApexChart1;
---
+++ src/Components/Element5.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Element5 = (props) => {
+    return (
+        <div class="timeline-item">
+            <i class="mdi mdi-upload bg-info-lighten text-info timeline-icon"></i>
+            <div class="timeline-item-info">
+                <a href="javascript:void(0);" class="text-info fw-bold mb-1 d-block">{props.doer}</a>
+                <small>{props.activity}</small>
+                <p class="mb-0 pb-2">
+                    <small class="text-muted">{props.time}</small>
+                </p>
+            </div>
+        </div>
+
+        
+
+
+    );
+
+}
+
+export default Element5;
