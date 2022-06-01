f760dd7c1506332102162ca6eb25da9fe94e1faa
PratyushAggarwal <pratyushaggarwal1281@gmail.com>
2022-06-01 14:32:04 +0530

    added opening a new tab while clicking the column of the projectile vs actual apex chart

--- src/Components/ApexChar1.js
+++ src/Components/ApexChar1.js
@@ -4,125 +4,124 @@ import {Navigate} from 'react-router-dom';
 import {useNavigate} from 'react-router-dom';
 
 
-class ApexChart1 extends Component {
-  
-  // navigate = useNavigate();
-  constructor(props) {
-    super(props);
-
-    var labelFormatter = function (value) {
-      var val = Math.abs(value);
-      if (val >= 1000) {
-        val = val / 1000 + "K";
-      }
-      return val;
-    };
-
-    var routeChange = function (index) {
-      console.log("hello");
-      return(
-        <Navigate to="/calender" />
-      );
-      // this.navigate('/calender');
-      // this.navigate('www.google.com');
-    }
-
-    this.state = {
-      series: [
-        {
-          name: "PRODUCT A",
-          data: [
-            144000, 55000, 41000, 67000, 22000, 43000, 144000, 55000, 41000,
-            67000, 22000, 43000,
-          ],
-        },
-        {
-          name: "PRODUCT B",
-          data: [
-            53000, 83000, 40000, 38000, 93000, 127000, 13000, 93000, 120000,
-            18000, 113000, 27000,
-          ],
-        },
-      ],
-      grid: {
-        show: true,
-        borderColor: "white",
-        strokeDashArray: 0,
-        position: "back",
-        chart: {
-          type: "bar",
-          height: "300px",
-          stacked: true,
-          events:{
-            click: (event, chartContext, config) =>{
-              console.log(event, chartContext, config);
-              routeChange(config.dataPointIndex);
-            }
-          },
-          toolbar: {
-            show: false,
-          },
-        },
-        plotOptions: {
-          bar: {
-            horizontal: false,
-            columnWidth: "18%",
-          },
-        },
-        xaxis: {
-          categories: [
-            "Jan",
-            "Feb",
-            "Mar",
-            "Apr",
-            "May",
-            "Jun",
-            "Jul",
-            "Aug",
-            "Sep",
-            "Oct",
-            "Nov",
-            "Dec",
-          ],
-          lines: {
-            show: true,
-          },
-        },
-        yaxis: {
-          max: 200000,
-          tickAmount: 5,
-          labels: {
-            formatter: labelFormatter,
-          },
-        },
-        fill: {
-          opacity: 1,
-        },
-        colors: ["#727cf5", "#E3EAEF"],
-        legend: {
-          show: false,
-        },
-        dataLabels: {
-          enabled: false,
-        },
-      },
-    };
+var labelFormatter = function (value) {
+  var val = Math.abs(value);
+  if (val >= 1000) {
+    val = val / 1000 + "K";
   }
+  return val;
+};
 
 
-  render() {
+
+
+
+
+const series = [
+  {
+    name: "PRODUCT A",
+    data: [
+      144000, 55000, 41000, 67000, 22000, 43000, 144000, 55000, 41000,
+      67000, 22000, 43000,
+    ],
+  },
+  {
+    name: "PRODUCT B",
+    data: [
+      53000, 83000, 40000, 38000, 93000, 127000, 13000, 93000, 120000,
+      18000, 113000, 27000,
+    ],
+  },
+];
+
+
+
+
+const ApexChart1 = () =>{
+  const navigate = useNavigate();
+
+  var routeChange = function (index) {
+    // let navigate = useNavigate();
+    console.log("hello");
+    window.open("https://www.youtube.com/watch?v=kMUdvZLljpM");
+    // this.navigate('/calender');
+    // this.navigate('www.google.com');
+  }
+  
+
+  const grid = {
+    show: true,
+    borderColor: "white",
+    strokeDashArray: 0,
+    position: "back",
+    chart: {
+      type: "bar",
+      height: "300px",
+      stacked: true,
+      events:{
+        click: (event, chartContext, config) =>{
+          console.log(event, chartContext, config);
+          routeChange(config.dataPointIndex);
+        }
+      },
+      toolbar: {
+        show: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "18%",
+      },
+    },
+    xaxis: {
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      lines: {
+        show: true,
+      },
+    },
+    yaxis: {
+      max: 200000,
+      tickAmount: 5,
+      labels: {
+        formatter: labelFormatter,
+      },
+    },
+    fill: {
+      opacity: 1,
+    },
+    colors: ["#727cf5", "#E3EAEF"],
+    legend: {
+      show: false,
+    },
+    dataLabels: {
+      enabled: false,
+    },
+  };
     return (
       <div id="chart">
         <ReactApexChart
-          options={this.state.grid}
-          series={this.state.series}
+          options={grid}
+          series={series}
           type="bar"
           height={256}
           width="100%"
         />
       </div>
     );
-  }
 }
 
 export default ApexChart1;
